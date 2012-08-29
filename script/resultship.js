a8ee170ca1aaabb3b62ca15859f1a5d3aa5176b7
chenzhao <remaintears@gmail.com>
2012-08-29 09:25:19 +0800

    neworder

--- script/resultship.js
+++ script/resultship.js
@@ -11,23 +11,58 @@ $(function(){
 
 	$('.btn_bts').click(function(e){
 		e.preventDefault();
-		var boat=$("#boatname").val();
-		var date=$("#shipcal").val();
+		var boat=$(this).parent().prev('.boatinfo').children('.cont').children('h4').children('a').text();
+		var date=$(this).attr("alt");
 		var arrdate=date.split('-');
-		var dnow=$(this).children("span").text();
-		var t=new Date(Date.UTC(arrdate[0],arrdate[1]-1,dnow));
-		var sd=$(this).children('.rday').val();
-		var t = parseInt((t.getTime())/1000)+sd*3600*24;
-		var data=new Date(parseInt(t)*1000);
-		var ed=data.getFullYear()+"-"+(data.getMonth()+1)+"-"+data.getDate();
 		var montharray=new Array("","Jan","Feb", "Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"); 
-		var arrdateend=ed.split('-');
-		var price=$(this).children("i").text();
-		var route=$(this).children(".route").val();
+		
+		var price=$(this).prev('.jg').text();
+	
 		var ms=parseInt(arrdate[1],10);
-		var me=parseInt(arrdateend[1],10);
-		var hideform='<input type="hidden" id="Boatname" class="txt" value="'+boat+'" name="boatname"><input type="hidden" name="Price" value="'+price+'"><input type="hidden" id="route" class="txt" name="Route" value="'+route+'"><input type="hidden" id="date" class="txt" name="Date" value="'+montharray[ms]+'.'+dnow+'.'+arrdate[0]+' - '+montharray[me]+'.'+arrdateend[2]+'.'+arrdateend[0]+'">';
-		var str='<ul><li><h3>Your Booking Summary</h3></li><li><strong>Cruise ship:</strong> '+boat+'</li><li><strong>Price:</strong> '+price+' per person</li><li><strong>Cruise date:</strong> '+montharray[ms]+'.'+dnow+'.'+arrdate[0]+' - '+montharray[me]+'.'+arrdateend[2]+'.'+arrdateend[0]+'</li><li><strong>Itinerary:</strong> '+route+'</li></ul>';
+		
+		var hideform='<input type="hidden" id="Boatname" class="txt" value="'+boat+'" name="boatname"><input type="hidden" name="Price" value="'+price+'"><input type="hidden" id="date" class="txt" name="Date" value="'+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'">';
+		var str='<ul><li><h3>Your Booking Summary</h3></li><li><strong>Cruise ship:</strong> '+boat+'</li><li><strong>Price:</strong> '+price+' per person</li><li><strong>Cruise date:</strong> '+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'</li></ul>';
+		$(".formrightinfo").html(str);
+		$(".hideninfo").html(hideform);
+		$(".cruiseform").fadeIn("fast");
+		 var docheight = $(document).height();
+		 $("body").append("<div id='greybackground'></div>");
+		 $("#greybackground").css({"opacity":"0.5","height":docheight});
+	});	
+
+	$('.ups').click(function(e){
+		e.preventDefault();
+		var boat=$(this).parent().siblings('h4').children('a').text();
+		var date=$(this).attr("alt");
+		var arrdate=date.split('-');
+		var montharray=new Array("","Jan","Feb", "Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"); 
+		
+		var price=$(this).parent().parent('.cont').parent('.boatinfo').next('.price').children('.jg').text();
+	
+		var ms=parseInt(arrdate[1],10);
+		
+		var hideform='<input type="hidden" id="Boatname" class="txt" value="'+boat+'" name="boatname"><input type="hidden" name="Price" value="'+price+'"><input type="hidden" id="date" class="txt" name="Date" value="'+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'">';
+		var str='<ul><li><h3>Your Booking Summary</h3></li><li><strong>Cruise ship:</strong> '+boat+'</li><li><strong>Price:</strong> '+price+' per person</li><li><strong>Cruise date:</strong> '+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'</li></ul>';
+		$(".formrightinfo").html(str);
+		$(".hideninfo").html(hideform);
+		$(".cruiseform").fadeIn("fast");
+		 var docheight = $(document).height();
+		 $("body").append("<div id='greybackground'></div>");
+		 $("#greybackground").css({"opacity":"0.5","height":docheight});
+	});	
+	$('.downs').click(function(e){
+		e.preventDefault();
+		var boat=$(this).parent().siblings('h4').children('a').text();
+		var date=$(this).attr("alt");
+		var arrdate=date.split('-');
+		var montharray=new Array("","Jan","Feb", "Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"); 
+		
+		var price=$(this).parent().parent('.cont').parent('.boatinfo').next('.price').children('.jg').text();
+	
+		var ms=parseInt(arrdate[1],10);
+		
+		var hideform='<input type="hidden" id="Boatname" class="txt" value="'+boat+'" name="boatname"><input type="hidden" name="Price" value="'+price+'"><input type="hidden" id="date" class="txt" name="Date" value="'+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'">';
+		var str='<ul><li><h3>Your Booking Summary</h3></li><li><strong>Cruise ship:</strong> '+boat+'</li><li><strong>Price:</strong> '+price+' per person</li><li><strong>Cruise date:</strong> '+montharray[ms]+'.'+arrdate[2]+'.'+arrdate[0]+'</li></ul>';
 		$(".formrightinfo").html(str);
 		$(".hideninfo").html(hideform);
 		$(".cruiseform").fadeIn("fast");
